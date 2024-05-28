b3750de94dd093b671637be56747300da21b8b50
Raj Thombare <therajthombare@gmail.com>
2024-05-28 10:27:56 +0530

    added sidebar in user-app

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
+import { AppbarClient } from "./components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en">
+    <html lang='en'>
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <div className='min-w-screen min-h-screen bg-[#ebe6e6]'>
+            <AppbarClient />
+            {children}
+          </div>
+        </body>
       </Providers>
     </html>
   );
