3afc0f501626b558340f32b263c6b3d006ec02d0
Raj Thombare <therajthombare@gmail.com>
2024-06-02 13:01:17 +0530

    fix: merchant user create

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server"
 import db from "@repo/db/client";
 
 export const GET = async () => {
-    await db.merchant.create({
+    await db.user.create({
         data: {
-            email: "asd",
-            name: "adsads",
-            auth_type: "Google",
+            email: "merchant@gmail.com",
+            name: "Merchant 1",
+            number: "3333333333",
+            password: "merchant"
         }
     })
     return NextResponse.json({
